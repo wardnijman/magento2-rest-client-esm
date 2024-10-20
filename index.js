9764074e35ad6424ec54cf1791225d50e2ee8b83
Ward Nijman <wardnijman@gmail.com>
2024-10-20 17:55:24 +0200

    Converted with cjstoesm

--- index.js
+++ index.js
@@ -1,41 +1,36 @@
+import { RestClient as RestClient$0 } from "./lib/rest_client.js";
+import categories from "./lib/categories.js";
+import attributes from "./lib/attributes.js";
+import products from "./lib/products.js";
+import productMedia from "./lib/product_media.js";
+import categoryProducts from "./lib/category_products.js";
+import configurableChildren from "./lib/configurable_children.js";
+import configurableOptions from "./lib/configurable_options.js";
+import taxRates from "./lib/tax_rates.js";
+import taxRules from "./lib/tax_rules.js";
+import stockItems from "./lib/stock_items.js";
+import customers from "./lib/customers.js";
+import directory from "./lib/directory.js";
+import cart from "./lib/cart.js";
+import orders from "./lib/orders.js";
+import reviews from "./lib/reviews.js";
 'use strict';
-
-var RestClient = require('./lib/rest_client').RestClient;
-var categories = require('./lib/categories');
-var attributes = require('./lib/attributes');
-var products = require('./lib/products');
-var productMedia = require('./lib/product_media');
-var categoryProducts = require('./lib/category_products');
-var configurableChildren = require('./lib/configurable_children');
-var configurableOptions = require('./lib/configurable_options');
-var taxRates = require('./lib/tax_rates');
-var taxRules = require('./lib/tax_rules');
-var stockItems = require('./lib/stock_items');
-var customers = require('./lib/customers');
-var directory = require('./lib/directory');
-var cart = require('./lib/cart');
-var orders = require('./lib/orders');
-var reviews = require('./lib/reviews');
-
+var RestClient = { RestClient: RestClient$0 }.RestClient;
 const MAGENTO_API_VERSION = 'V1';
-
-module.exports.Magento2Client = function (options) {
+export const Magento2Client = function (options) {
     var instance = {
-        addMethods (key, module) {
+        addMethods(key, module) {
             var client = RestClient(options);
             if (module) {
                 if (this[key])
-                    this[key] = Object.assign(this[key], module(client))
-                else 
-                    this[key] = module(client)
+                    this[key] = Object.assign(this[key], module(client));
+                else
+                    this[key] = module(client);
             }
         }
     };
-
     options.version = MAGENTO_API_VERSION;
-    
     var client = RestClient(options);
-
     instance.attributes = attributes(client);
     instance.categories = categories(client);
     instance.products = products(client);
@@ -51,6 +46,5 @@ module.exports.Magento2Client = function (options) {
     instance.orders = orders(client);
     instance.directory = directory(client);
     instance.reviews = reviews(client);
-
     return instance;
-}
+};
